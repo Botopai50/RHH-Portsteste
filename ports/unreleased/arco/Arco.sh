8d2c5b19a9c038cdd1078b781ce367fdeca29a56
Jeod <47716344+JeodC@users.noreply.github.com>
2025-10-04 09:39:14 -0400

    [ARCO] Adjust unreleased port to compress audio, add note for testing

--- ports/unreleased/arco/Arco.sh
+++ ports/unreleased/arco/Arco.sh
@@ -28,13 +28,28 @@ $ESUDO chmod +x -R $GAMEDIR/*
 export LD_LIBRARY_PATH="$GAMEDIR/libs.${DEVICE_ARCH}:$LD_LIBRARY_PATH"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
+# Check if we need to patch the game
+if [ ! -f patchlog.txt ]; then
+    if [ -f "$controlfolder/utils/patcher.txt" ]; then
+        export PATCHER_FILE="$GAMEDIR/tools/patchscript"
+        export PATCHER_GAME="$(basename "${0%.*}")"
+        export PATCHER_TIME="10 to 15 minutes"
+        export controlfolder
+        export ESUDO
+        source "$controlfolder/utils/patcher.txt"
+        $ESUDO kill -9 $(pidof gptokeyb)
+    else
+        echo "This port requires the latest version of PortMaster."
+    fi
+fi
+
 # Config
-mkdir -p $"GAMEDIR/save"
+mkdir -p "$GAMEDIR/save"
 bind_directories "$XDG_DATA_HOME/love/arco/save" "$GAMEDIR/save"
 
 # Run game
 $GPTOKEYB "love" -c "arco.gptk" & 
-pm_platform_helper "$GAMEDIR/love"
+pm_platform_helper "$GAMEDIR/love" > /dev/null
 ./love "$GAMEDIR/arco.exe"
 
 # Cleanup
--- ports/unreleased/arco/README.md
+++ ports/unreleased/arco/README.md
@@ -2,8 +2,7 @@
 Purchase the game on Steam and copy `arco.exe` to `ports/arco`.
 
 ## Notes
-This port demands high performance and is tagged for "ultra". It runs well on the Retroid Pocket 5 and Mini, but other devices are untested.
+This unreleased port recompresses audio for the game, reducing the memory footprint. It demands around 600MB of RAM at the title screen, so given cfw overhead 1GB devices might be right on the cusp. Test at your own risk.
 
 ## Thanks 
 Developers Franek, Max Cahill, Bibiki, Fáyer for the amazing game  
-Testers & Devs from the PortMaster Discord
