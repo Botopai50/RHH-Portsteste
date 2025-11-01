ea9ff8417f30dadef3add01afbe68c054eb87d7c
Jeod <47716344+JeodC@users.noreply.github.com>
2025-10-31 23:57:18 -0400

    [UNRELEASED] Add unreleased port Momodora RUtM

---
+++ ports/unreleased/momodora_rutm/Momodora RUtM.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+export PORT_32BIT="Y"
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+# Variables
+GAMEDIR="/$directory/ports/momodora_rutm"
+GAME="$GAMEDIR/gamedata/MomodoraRUtM"
+BOX86="$GAMEDIR/box86/box86"
+
+# CD and set log
+cd $GAMEDIR
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+# Exports
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+export LD_LIBRARY_PATH="$GAMEDIR/libs.armhf:/usr/lib32:$LD_LIBRARY_PATH"
+export BOX86_LD_LIBRARY_PATH="$GAMEDIR/box86/x86"
+
+# Bind config dir
+bind_directories ~/.config/MomodoraRUtM "$GAMEDIR/config"
+
+# Prepare game
+find "$GAMEDIR/gamedata" -type f \( \
+    -name "*.sh" -o -name "*.so" -o -name "*.out" \
+\) -exec rm -f {} \;
+
+# Mount Weston runtime
+weston_dir=/tmp/weston
+$ESUDO mkdir -p "$weston_dir"
+weston_runtime="weston_pkg_0.2"
+
+if [ ! -f "$controlfolder/libs/${weston_runtime}.squashfs" ]; then
+	if [ ! -f "$controlfolder/harbourmaster" ]; then
+		pm_message "This port requires the latest PortMaster to run, please go to https://portmaster.games/ for more info."
+		sleep 5
+		exit 1
+	fi
+
+	# Try quiet install
+	if ! $ESUDO "$controlfolder/harbourmaster" --quiet --no-check runtime_check "${weston_runtime}.squashfs"; then
+		pm_message "Failed to install the runtime automatically. Please update PortMaster or install '${weston_runtime}' manually."
+		sleep 5
+		exit 1
+	fi
+fi
+
+if [ "$PM_CAN_MOUNT" != "N" ]; then
+	$ESUDO umount "$weston_dir"
+fi
+
+$ESUDO mount "$controlfolder/libs/${weston_runtime}.squashfs" "$weston_dir"
+
+# Run it
+cd "$GAMEDIR/gamedata"
+$GPTOKEYB "MomodoraRUtM" -c "$GAMEDIR/momodora.gptk" & 
+pm_platform_helper "$GAME" > /dev/null
+$ESUDO env WRAPPED_LIBRARY_PATH="$GAMEDIR/libs.armhf" $weston_dir/westonwrap.sh headless noop kiosk crusty_x11egl \
+"$BOX86" "$GAME"
+
+# Clean up
+$ESUDO $weston_dir/westonwrap.sh cleanup
+if [ "$PM_CAN_MOUNT" != "N" ]; then
+	$ESUDO umount "$weston_dir"
+fi
+pm_finish
---
+++ ports/unreleased/momodora_rutm/momodora_rutm/README.md
@@ -0,0 +1,22 @@
+## Installation
+Download the linux version and add game files to `momodora_rutm/gamedata`.
+
+## Default Gameplay Controls
+| Button            | Action |
+|--                 |--|
+| START             | Menu |
+| D-PAD / JOYSTICK  | Move |
+| A                 | Jump |
+| B                 | Roll |
+| X                 | Melee Attack |
+| Y                 | Item |
+| L1                | Switch Item |
+| R1                | Ranged Attack |
+| L2 (Held)         | Map |
+
+## Notes
+For the time being this port requires a decent cpu (`power`) due to short-lived frame drops when particle effects appear on screen. This will be fine in early gameplay, but later boss fights will be more prolonged.
+
+## Thanks
+bombservice -- The game  
+JohnnyOnFlame -- Initial port work 
