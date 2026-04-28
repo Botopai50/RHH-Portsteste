43ebf5ca68f92b14dbc92f40174fedeceda37a97
Jeod <47716344+JeodC@users.noreply.github.com>
2026-04-28 12:36:14 -0400

    [ELEMENTALLIS] Release Elementallis

---
+++ ports/released/boxports/elementallis/Elementallis.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+# Variables
+GAMEDIR="/$directory/ports/elementallis"
+GAME="$GAMEDIR/data/Elementallis.x86_64"
+BOX64="$GAMEDIR/box64/box64"
+
+# CD and set log
+cd $GAMEDIR/data
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+# Pre-flight checks for X11 and OpenGL
+if [ -z "$DISPLAY" ]; then
+    echo "Error: Display manager not found. This game requires OpenGL and X11 to run."
+    exit 1
+fi
+
+if ! command -v glxinfo >/dev/null 2>&1; then
+    echo "Error: OpenGL not found. This game requires OpenGL and X11 to run."
+    exit 1
+fi
+
+# Exports
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+export LD_LIBRARY_PATH="$GAMEDIR/box64/x64:$GAMEDIR/libs.aarch64:$GAMEDIR/data:$LD_LIBRARY_PATH"
+export BOX64_LD_LIBRARY_PATH="$GAMEDIR/box64/x64:$GAMEDIR/data:$LD_LIBRARY_PATH"
+export XDG_CONFIG_HOME="$GAMEDIR/config" && mkdir -p "$GAMEDIR/config"
+
+export SDL_VIDEODRIVER=x11
+
+# Box64 settings
+export BOX64_NOBANNER=1
+export BOX64_DYNAREC=1
+export BOX64_DYNAREC_SAFEFLAGS=0
+export BOX64_DYNAREC_FASTROUND=1
+export BOX64_DYNAREC_BIGBLOCK=1
+export BOX64_DYNAREC_CALLRET=1
+export BOX64_DYNAREC_DIRTY=1
+export BOX64_DYNAREC_FORWARD=128
+export BOX64_RDTSC_1GHZ=1
+export BOX64_VSYNC=0
+
+# Display loading splash
+[ "$CFW_NAME" == "muOS" ] && $ESUDO "$GAMEDIR/tools/splash" "$GAMEDIR/splash.png" 1 
+$ESUDO "$GAMEDIR/tools/splash" "$GAMEDIR/splash.png" 30000 &
+
+# Run it
+$GPTOKEYB "$GAME" xbox360 & 
+pm_platform_helper $GAME > /dev/null
+$BOX64 "$GAME"
+
+#Clean up after ourselves
+pm_finish
---
+++ ports/released/boxports/elementallis/elementallis/README.md
@@ -0,0 +1,7 @@
+## Installation
+This port uses the Linux game assets with Box64. Wine is not involved. Copy your game assets to the `data` folder.
+
+If you own the Steam version, you can use https://steamdb.info/depot/1304283/manifests/ to grab the latest depot download link. Copy it into the Steam console to download the Linux package.
+
+## Requirements
+You must be using a device with desktop OpenGL and have access to x11. This port was tested on Rocknix RPMini and RPFlip2. These are mainline linux devices.
